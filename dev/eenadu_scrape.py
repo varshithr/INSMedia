9f33c3c9f6f353a2c978f49a84ff730489fcbe20
Venkatesh <venkatesh.paruchuri@scriptbees.com>
2017-03-17 16:36:35 +0530

    added

--- dev/eenadu_scrape.py
+++ dev/eenadu_scrape.py
@@ -10,15 +10,22 @@ from psycopg2 import connect
 from requests import get
 import requests.packages.urllib3
 requests.packages.urllib3.disable_warnings()
+import logging
 
+logging.basicConfig(filename='eenadu_scrape.log',level=logging.DEBUG)
+
+
+logging.info('trying to establish connection with database')
 conn = connect(database="insmedia", user="postgres",
                         password="scriptbees1$", host="127.0.0.1", port="5432")
 cursor = conn.cursor()
+logging.debug('connection estabished successfully')
 
 
 def scrape():
     #Change url_inserted_date every week here
-    query1 = """select s_no, newsitem_link from posts where 
+    logging.info('entered into scrape function in eenadu_scrape')
+    query1 = """select s_no, newsitem_link from posts where
             paper = 'eenadu' and
             url_inserted_date = current_date""" #+ """ and s_no = 2"""
     cursor.execute(query1)
@@ -27,12 +34,14 @@ def scrape():
         s_no = item[0]
         url = item[1]
         print url
+        logging.debug(url)
         #url = url.split('..')
         if url.startswith('/'):
             url = 'http://www.eenadu.net' + url
         else:
             url = 'http://www.eenadu.net/' + url
         print s_no, url
+        logging.debug(str(s_no),url)
         page = get(url)
         if 200 != page.status_code:
             print 'failed getting site url data ' + url
@@ -45,13 +54,16 @@ def scrape():
             #print content
             l = l + content
         print l, len(l)
+        logging.debug(l)
         l = l.encode('utf-8')
         updatequery = "update posts set article_content = %s where s_no = %s"
         cursor.execute(updatequery,(l, str(s_no)))
-        conn.commit() 
+        conn.commit()
+    logging.debug('leaving scrape function.')
     return True
 
 def eenadu_scrape():
+    logging.info('this is main function of in eenadu_scrape')
     result = scrape()
     if not result:
          print 'There was an error!'
@@ -59,4 +71,5 @@ def eenadu_scrape():
          print (r"scrape job for the site www.eenadu.net has been done")
 
 if __name__ == '__main__':
+    logging.info('calling eenadu_scrape main function')
     eenadu_scrape()
