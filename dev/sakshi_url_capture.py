9e255f5055e8f522f615b842f5ef51d8e8265324
madansirish <madan.sirish63@gmail.com>
2017-03-22 12:18:05 +0530

    sakshi url capture

--- dev/sakshi_url_capture.py
+++ dev/sakshi_url_capture.py
@@ -18,17 +18,21 @@ from requests import get
 from psycopg2 import connect
 from datetime import date
 import os
+import logging
 
-path=getcwd()
+path=os.path.abspath(os.path.join(os.getcwd(), os.pardir))+'/logs'
 
-
+logging.basicConfig(filename=path+'/'+'sakshi_url_capture.log',level=logging.DEBUG)
+logging.info('trying to establish a connection with database')
 conn = connect(database="insmedia", user="postgres",
                         password="scriptbees1$", host="127.0.0.1", port="5432")
 cursor = conn.cursor()
+logging.debug('connection established successfully')
 home = 'http://www.sakshi.com/'
 today = date.today()
 
 def newslist(page, sliderpage, url):
+    logging.info('entered into newslist function')
     if sliderpage:
         return False
     else:
@@ -51,9 +55,11 @@ def newslist(page, sliderpage, url):
             link = i.get('href')
             print link
             insertsakshi(link,classifier)
+    logging.debug('leaving newslist function')
     return True;
 
 def slider(page, sliderpage):
+    logging.info('entered into slider')
     classifier = 'slider'
     if not sliderpage:
         return False
@@ -72,11 +78,14 @@ def slider(page, sliderpage):
             newsitem_link = a.get('href')
             print newsitem_link, classifier
             insertsakshi(newsitem_link, classifier)
+    logging.info('leaving slider function')
     return True
 
 def hotnews():
+    logging.info('entered into hotnews method')
     page = get(home)
     if 200 != page.status_code:
+        logging.error('failed getting data of page '+home)
         print 'failed getting data of page ' + home
         return False
     classifier = "hotnews"
@@ -90,17 +99,21 @@ def hotnews():
         newsitem_link = a.get('href')
         print newsitem_link, classifier
         insertsakshi(newsitem_link, classifier)
+    logging.debug('leaving hotnews function')
     return True
 
 def insertsakshi(newsitem_link, classifier):
+    logging.info('entered into insertsakshi function')
     paper = 'sakshi'
     query = """INSERT INTO posts (newsitem_link,classifier, 
     url_inserted_date, paper) values (%s,%s,%s,%s)"""
     cursor.execute(query,(newsitem_link,  classifier, today, paper))
     conn.commit()
+    logging.debug('inserted rows successfully into db')
 
 
 def sakshi():
+    logging.info('entered into sakshi function')
     query = """select link,sliderpage from sakshi_menu""" #+ """ where s_no in (1)"""
     cursor.execute(query)
     urls = cursor.fetchall()
@@ -108,7 +121,8 @@ def sakshi():
         page = get(url[0])
         print url[0]
         if 200 != page.status_code:
-            print 'failed getting data of page ' + url
+            print 
+            logging.debug('failed getting data of page ' + url)
             continue
         result = slider(page,url[1])
         result = newslist(page,url[1], url[0])
@@ -117,7 +131,9 @@ def sakshi():
     result = hotnews()
     if not result:
              pass
+    logging.debug("Content updated to the Postgres table 'posts'")
     print r"Content updated to the Postgres table 'posts'"
 
 if __name__ == '__main__':
+    logging.info('calling main function in sakshi_url_capture ')
     sakshi()
